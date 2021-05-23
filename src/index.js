f7c83d423a82e07703c1a4c5611578f417086183
23birola <23birola@gmail.com>
2021-05-23 18:18:56 +0300

    edit code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import galleryTemplate from './templates/gallery';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    buttonLoadMore: document.querySelector('.loadMoreButton')
+}
