76af45035c076d785027db23e778bb6d93c7065a
23birola <23birola@gmail.com>
2021-05-23 22:50:15 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import galleryTemplate from './templates/gallery';
 const refs = {
     searchForm: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
-    buttonLoadMore: document.querySelector('.loadMoreButton')
+    buttonLoadMore: document.querySelector('.loadMoreButton'),
+    buttonSearchImg: document.querySelector('.submit')
+}
+
+async function searchImage {
+    const response = await fetch(https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ)
 }
